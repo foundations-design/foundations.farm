1508d7e8bb0a4a990be5319cb2ed36688b9fe06a
earlman <earl.mangulabnan@gmail.com>
2019-04-29 10:26:54 -0500

    refactored _layout.scss

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -10,7 +10,12 @@
 				<g-link class="nav__link" to="/posts">Contact Us</g-link>
 			</nav>
 		</header>
-		<slot/>
+		<main>
+			<slot/>
+		</main>
+		<footer>
+			<div>Copyright 2019.</div>
+		</footer>
 	</div>
 </template>
 
@@ -33,31 +38,4 @@ query {
 </static-query>
 
 <style lang="scss">
-body {
-	font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI",
-		Roboto, "Helvetica Neue", Arial, sans-serif;
-	font-size: $baseFontSize;
-	margin: 0;
-	padding: 0;
-	line-height: 1.5;
-}
-
-.layout {
-	max-width: 760px;
-	margin: 0 auto;
-	padding-left: 20px;
-	padding-right: 20px;
-
-	.header {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		margin-bottom: 20px;
-		height: 80px;
-	}
-}
-
-.nav__link {
-	margin-left: 20px;
-}
 </style>
