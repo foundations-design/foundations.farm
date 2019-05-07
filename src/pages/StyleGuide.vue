3d138fef6d0fe5924c0c4e946245b9fa9c01aa5a
earlman <earl.mangulabnan@gmail.com>
2019-05-07 12:24:57 -0500

    add colors and styleguide colors

--- src/pages/StyleGuide.vue
+++ src/pages/StyleGuide.vue
@@ -4,11 +4,32 @@
 		<div>
 			<h1>Style Guide</h1>
 		</div>
+		<div class="color">
+			<div class="section-title">
+				<h2>Color</h2>
+			</div>
+			<div>
+				<h3>Main</h3>
+				<div class="color-box-container">
+					<div class="color-box color-primary"></div>
+					<div class="color-box color-accent-light"></div>
+					<div class="color-box color-accent-dark"></div>
+				</div>
+			</div>
+			<div>
+				<h3>Text</h3>
+				<div class="color-box-container">
+					<div class="color-box color-text-heading"></div>
+					<div class="color-box color-text-body"></div>
+					<div class="color-box color-text-caption"></div>
+				</div>
+			</div>
+		</div>
 
 		<div class="typography">
-			<p>Typography is the bedrock of web design</p>
-			<div class="section_title">
+			<div class="section-title">
 				<h2>Typography</h2>
+				<p class="text-caption">Typography is the bedrock of web design</p>
 			</div>
 			<h1>This is an h1 Heading</h1>
 			<h2>This is an h2 Heading</h2>
@@ -17,7 +38,7 @@
 			<h5>This is an h5 Heading</h5>
 			<h6>This is an h6 Heading</h6>
 
-			<p class="text-button">button text</p>
+			<p class="text-button">Button Text</p>
 			<p class="text-caption">This is the caption text 00</p>
 		</div>
 	</div>
@@ -33,7 +54,43 @@ body {
 	grid-column: 2;
 }
 
-.section_title {
+.section-title {
 	background-color: #a5adba;
 }
+
+.color-box-container {
+	display: flex;
+
+	.color-box {
+		width: 200px;
+		height: 100px;
+		border: 1px gray solid;
+		margin: 1em;
+	}
+
+    .color-primary {
+    background-color: var(--color-primary)
+    }
+
+    .color-accent-light {
+        background-color: var(--color-accent-light)
+    }
+
+    .color-accent-dark {
+        background-color: var(--color-accent-dark)
+    }
+
+    .color-text-heading {
+        background-color: var(--color-text-heading)
+    }
+
+    .color-text-body {
+        background-color: var(--color-text-body)
+    }
+
+    .color-text-caption {
+        background-color: var(--color-text-caption)
+    }
+}
+
 </style>
