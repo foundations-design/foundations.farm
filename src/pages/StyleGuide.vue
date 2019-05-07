1e7e833ebbd253dc57a9bd19a963fc6e18949826
earlman <earl.mangulabnan@gmail.com>
2019-05-07 11:24:12 -0500

    updating typography styles

--- src/pages/StyleGuide.vue
+++ src/pages/StyleGuide.vue
@@ -1,11 +1,15 @@
 <template>
 	<!-- Do not use vue-specific functionality here without an html-only backup -->
 	<div class="content">
-		<h1>Style Guide</h1>
+		<div>
+			<h1>Style Guide</h1>
+		</div>
 
 		<div class="typography">
 			<p>Typography is the bedrock of web design</p>
-			<h2>Typography</h2>
+			<div class="section_title">
+				<h2>Typography</h2>
+			</div>
 			<h1>This is an h1 Heading</h1>
 			<h2>This is an h2 Heading</h2>
 			<h3>This is an h3 Heading</h3>
@@ -17,14 +21,16 @@
 </template>
 
 <style lang="scss">
-    body {
-        display: grid;
-        grid-template-columns: 1fr 1200px 1fr;
-        
-    }
+body {
+	display: grid;
+	grid-template-columns: 1fr 1200px 1fr;
+}
 
-    .content {
-        grid-column: 2;
-    }
+.content {
+	grid-column: 2;
+}
 
+.section_title {
+	background-color: #a5adba;
+}
 </style>
