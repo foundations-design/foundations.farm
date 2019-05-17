be26f50d4c746ed5e5fec7ab4d428aa739368bb0
earlman <earl.mangulabnan@gmail.com>
2019-05-17 10:32:21 -0500

    directly copied gridsome form

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -69,40 +69,39 @@
 				<button type="submit" class="button right">Send</button>
 			</form>
 		</section>
-		<!--<section id="test-form">
-			<form name="contact" method="POST" data-netlify="true">
-				<p>
-					<label>
-						Your Name:
-						<input type="text" name="name">
-					</label>
-				</p>
-				<p>
-					<label>
-						Your Email:
-						<input type="email" name="email">
-					</label>
-				</p>
-				<p>
-					<label>
-						Your Role:
-						<select name="role[]" multiple>
-							<option value="leader">Leader</option>
-							<option value="follower">Follower</option>
-						</select>
-					</label>
-				</p>
-				<p>
-					<label>
-						Message:
-						<textarea name="message"></textarea>
-					</label>
-				</p>
-				<p>
-					<button type="submit">Send</button>
-				</p>
-			</form>
-		</section>-->
+		<form
+			name="gridsome-contact"
+			method="post"
+			v-on:submit.prevent="handleSubmit"
+			action="/success/"
+			data-netlify="true"
+			data-netlify-honeypot="bot-field"
+		>
+			<input type="hidden" name="form-name" value="contact">
+			<p hidden>
+				<label>
+					Don’t fill this out:
+					<input name="bot-field">
+				</label>
+			</p>
+			<div class="sender-info">
+				<div>
+					<label for="name" class="label">Your name</label>
+					<input type="text" name="name" v-model="formData.name">
+				</div>
+				<div>
+					<label for="email">Your email</label>
+					<input type="email" name="email" v-model="formData.email">
+				</div>
+			</div>
+
+			<div class="message-wrapper">
+				<label for="message">Message</label>
+				<textarea name="message" v-model="formData.message"></textarea>
+			</div>
+
+			<button type="submit">Submit form</button>
+		</form>
 	</Layout>
 </template>
 
