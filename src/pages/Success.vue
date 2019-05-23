82c9da474a96fe7a97980fa1a70716a93f73f071
earlman <earl.mangulabnan@gmail.com>
2019-05-23 13:26:29 -0500

    add success page

---
+++ src/pages/Success.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="success">
+		<g-link class="button home" to="/">&#8592; home</g-link>
+		<h2>Thanks for your time</h2>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.success {
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	flex-direction: column;
+
+	h2 {
+		margin: 30vh auto;
+	}
+}
+
+.button.home {
+	width: 100px;
+	margin: 20px 20px;
+}
+</style>
